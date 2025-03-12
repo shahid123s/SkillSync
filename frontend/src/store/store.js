2ddb3d9542806fa6b1b884c079051c5b1e51ce10
Benson B Varoor <bensonbaiju46@gmail.com>
2025-03-12 21:22:22 +0530

    redux setup with presist

---
+++ frontend/src/store/store.js
@@ -0,0 +1,25 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import userReducer from "./userSlice";
+import adminReducer from "./adminSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // uses localStorage for web
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["user", "admin"],
+};
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  admin: adminReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
+export default store;
